b5e7866ef2eda5523dacfa01cdc0d944b187ef8c
Daris Cappelletti <daris.cappelletti@regione.marche.it>
2022-10-31 00:03:41 +0100

    Aggiunti file curriculum

---
+++ Index.aspx
@@ -0,0 +1,16 @@
+﻿<%@ 
+    Title="Generator curriculum"
+    Page Language="C#" 
+    AutoEventWireup="true" 
+    CodeBehind="Index.aspx.cs" 
+    Inherits="PortFolio.CurriculumGenerator.Index" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container">
+        <div>
+            <h1>Generatore curriculum</h1>
+            <h2>Tool per la generazione automatica di un curriculum con un template preimpostato.</h2>
+        </div>
+    </div>
+</asp:Content>
+
---
+++ Index.aspx.designer.cs
@@ -0,0 +1,17 @@
+﻿//------------------------------------------------------------------------------
+// <generato automaticamente>
+//     Codice generato da uno strumento.
+//
+//     Le modifiche a questo file possono causare un comportamento non corretto e verranno perse se
+//     il codice viene rigenerato. 
+// </generato automaticamente>
+//------------------------------------------------------------------------------
+
+namespace PortFolio.CurriculumGenerator
+{
+
+
+    public partial class Index
+    {
+    }
+}
